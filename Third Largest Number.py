2312afc9a03ad389f344c6a8ec162721c90c30f5
Aishwarya Priya <43652720+aishwarya0408@users.noreply.github.com>
2019-01-27 21:57:46 +0530

    Add files via upload

---
+++ Third Largest Number.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def thirdLargest(arr, n):
+    if(n<3):
+        return(-1)
+    else:
+        arr=sorted(arr)
+        return(arr[n-3])
+m=int(input("Enter the size of array:"))
+print("Enter the array:")
+a=list(map(int,input().split()))
+thirdLargest(a,m)
+
+
+# In[ ]:
+
+
+
+
